8e61b4237a022f480338bb3f8995c6408017187b
Michelle <michelle.winkler@miichelle.moe>
2026-02-02 10:33:05 +0100

    fix: use webroot challenge with certbot

--- bbb-install.sh
+++ bbb-install.sh
@@ -1571,7 +1571,7 @@ HERE
 
     if [ -z "$PROVIDED_CERTIFICATE" ]; then
       if ! certbot --email "$EMAIL" --agree-tos --rsa-key-size 4096 -w /var/www/bigbluebutton-default/assets/ \
-           -d "$HOST" --deploy-hook "systemctl reload nginx" "${LETS_ENCRYPT_OPTIONS[@]}" certonly; then
+           -d "$HOST" --deploy-hook "systemctl reload nginx" --preferred-challenges http "${LETS_ENCRYPT_OPTIONS[@]}" certonly; then
         systemctl restart nginx
         err "Let's Encrypt SSL request for $HOST did not succeed - exiting"
       fi
